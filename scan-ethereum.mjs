2e4beca05e1561b29a0fa00162cdddb22e4ad65d
chenwei <chenwei767@gmail.com>
2021-04-11 21:41:47 +0800

    Check the existence of event.transactionHash in parami before send below tx to parami.

--- scan-ethereum.mjs
+++ scan-ethereum.mjs
@@ -23,6 +23,7 @@ async function main() {
 
 async function scanBlock(opts, api, moduleMetadata, admin, web3, contract, blockNum) {
     let [a, b] = [null, null];
+    let txInParami = null;
     // https://blockchain.oodles.io/dev-blog/event-listeners-in-web3-js/
     // const a = await contract.methods.balanceOf("0x47ac0fb4f2d84898e4d9e7b4dab3c24507a6d503").call();
     let events = await contract.getPastEvents({filter: {}, fromBlock: blockNum, toBlock: blockNum});
@@ -30,8 +31,11 @@ async function scanBlock(opts, api, moduleMetadata, admin, web3, contract, block
         // console.log(event);
         switch (event.event) {
             case 'Transfer':
-                // TODO: check the existence of event.transactionHash in parami before send below tx to parami.
-                if (event.returnValues.to === opts.ethHotWallet) {
+                // Sending multiple extrinsics with same parameters is no harmed.
+                // But for the sake of speeding up eth scanning, we should check
+                // the existence of `event.transactionHash` in Parami chain.
+                txInParami = await api.query.bridge.erc20Txs(event.transactionHash);
+                if (txInParami.isNone && event.returnValues.to === opts.ethHotWallet) {
                     [a, b] = waitTx(moduleMetadata);
                     await api.tx.bridge.transfer(
                         event.transactionHash,
@@ -42,15 +46,17 @@ async function scanBlock(opts, api, moduleMetadata, admin, web3, contract, block
                 }
                 break;
             case 'Withdraw':
-                // TODO: check the existence of event.transactionHash in parami before send below tx to parami.
-                [a, b] = waitTx(moduleMetadata);
-                await api.tx.bridge.withdraw(
-                    event.transactionHash,
-                    event.returnValues.from,
-                    event.returnValues.paramiaddr,
-                    event.returnValues.value,
-                ).signAndSend(admin, a);
-                await b();
+                txInParami = await api.query.bridge.erc20Txs(event.transactionHash);
+                if (txInParami.isNone) {
+                    [a, b] = waitTx(moduleMetadata);
+                    await api.tx.bridge.withdraw(
+                        event.transactionHash,
+                        event.returnValues.from,
+                        event.returnValues.paramiaddr,
+                        event.returnValues.value,
+                    ).signAndSend(admin, a);
+                    await b();
+                }
                 break;
             default:
                 break;
@@ -84,7 +90,7 @@ async function scan(opts, from_block) {
         try {
             // get the newest block number.
             let bestBlockNum = await web3.eth.getBlockNumber();
-            if (from_block === 0) from_block = bestBlockNum - opts.depth;
+            if (from_block === 0) from_block = bestBlockNum - opts.depth * 2;
 
             if (from_block <= bestBlockNum - opts.depth) {
                 console.log("bestBlockNum %s, targetBlockNum %s", bestBlockNum, from_block);
